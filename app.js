1d6dda5ebe1515502815283dc3bffacec31de577
Агапов Кирилл <mrcurill@gmail.com>
2021-05-13 11:47:07 +0300

    fix

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const config = require('config');
 const path = require('path');
-// const mongoose = require('mongoose');
-// const User = require('./models/User')
 
 const app = express();
 
